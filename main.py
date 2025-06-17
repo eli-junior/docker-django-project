aedc3aa72a2e194cb2edaa4f2e6b033ff1119421
Eli Junior <elijunior.py@gmail.com>
2025-06-17 11:05:08 -0300

    uv

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from docker-django-project!")
+
+
+if __name__ == "__main__":
+    main()
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
+    path("polls/", include("mysite.polls.urls")),
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
 
